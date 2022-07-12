c3f51af85b855dd6ee01d3339d78bdef57cbfac3
H-HChen <annhunter201@gmail.com>
2022-07-12 19:43:54 +0800

    Modify Docker related file for ompl

--- docker/requirement/setup_repo.sh
+++ docker/requirement/setup_repo.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
-git clone https://github.com/H-HChen/GA-DDPG.git --recursive -b develop
-pip3 install "git+git://github.com/erikwijmans/Pointnet2_PyTorch.git#egg=pointnet2_ops&subdirectory=pointnet2_ops_lib"
-ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
-cd /GA-DDPG/OMG/ycb_render && python3 setup.py develop
-cd /GA-DDPG/OMG/Sophus
-cmake -Bbuild -H.
-cmake --build build/ -j8
-cd build && make install
-ls /usr/lib/python3.8/site-packages/
-cd /GA-DDPG/OMG/orocos_kinematics_dynamics
-cd ./sip-4.19.3
-python3 configure.py
-make -j8; make install
-export ROS_PYTHON_VERSION=3
-cd /GA-DDPG/OMG/orocos_kinematics_dynamics/orocos_kdl
-mkdir build; cd build;
-cmake ..
-make -j8; make install
-cd /GA-DDPG/OMG/orocos_kinematics_dynamics/python_orocos_kdl
-mkdir build; cd build;
-cmake ..  -DPYTHON_VERSION=3.8.10 -DPYTHON_EXECUTABLE=/usr/bin/python3
-make -j8; cp PyKDL.so /usr/lib/python3/dist-packages/
-cd /GA-DDPG/OMG/layers
-python3 setup.py install
+SUDO="sudo -H"
+
+# install most dependencies via apt-get
+${SUDO} apt-get -y update
+${SUDO} apt-get -y upgrade
+# We explicitly set the C++ compiler to g++, the default GNU g++ compiler. This is
+# needed because we depend on system-installed libraries built with g++ and linked
+# against libstdc++. In case `c++` corresponds to `clang++`, code will not build, even
+# if we would pass the flag `-stdlib=libstdc++` to `clang++`.
+${SUDO} apt-get -y install g++ cmake pkg-config libboost-serialization-dev libboost-filesystem-dev libboost-system-dev libboost-program-options-dev libboost-test-dev libeigen3-dev libode-dev wget libyaml-cpp-dev
+export CXX=g++
+export MAKEFLAGS="-j `nproc`"
+
+${SUDO} apt-get -y install python3-dev python3-pip
+# install additional python dependencies via pip
+${SUDO} pip3 install -vU https://github.com/CastXML/pygccxml/archive/develop.zip pyplusplus
+# install castxml
+${SUDO} apt-get -y install castxml
+${SUDO} apt-get -y install libboost-python-dev
+${SUDO} apt-get -y install libboost-numpy-dev python${PYTHONV}-numpy
+${SUDO} apt-get -y install pypy3
+
+git clone https://github.com/ompl/ompl.git
+cd ompl
+mkdir -p build/Release
+cd build/Release
+cmake ../..
+make -j 4 update_bindings
+make -j 4
+sudo make install
 cp -r /usr/lib/python3.8/site-packages/* /usr/lib/python3/dist-packages/
